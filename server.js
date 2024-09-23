07f694d12c86e8c68dc82f5646865c3bd75b115d
kaustubh mane <kaustubh6220@gmail.com>
2024-09-23 16:19:01 +0530

    port

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ app.post('/api/payment/verify', async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 80;  
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
